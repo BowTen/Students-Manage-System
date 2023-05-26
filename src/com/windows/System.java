6bb3a20efe2796eb4dbf0a3b87636ed097f7a571
BowTen <3182921986@qq.com>
2023-05-26 21:30:49 +0800

    5/26

--- src/com/windows/System.java
+++ src/com/windows/System.java
@@ -2,7 +2,7 @@ package com.windows;
 
 import java.io.IOException;
 
-public class StudentSystem {
+public class System {
     public static void main(String[] args) throws IOException {
         //创建登录界面对象（程序入口）
         LoginStart loginWindow = new LoginStart();
